8862a7105f833380a6914fc2e08861f735d2cd19
hecomi <info@hecomi.com>
2015-05-30 22:26:43 +0900

    fetch camera image asynchronously.

--- opencv/camera.cpp
+++ opencv/camera.cpp
@@ -1,13 +1,110 @@
+#include <chrono>
+#include <mutex>
 #include "camera.h"
 
 using namespace Littai;
 
 
 
+namespace {
+    std::mutex mutex;
+}
+
+
+
+// ---
+
+
+
+Camera::Fetcher::Fetcher(cv::VideoCapture &video)
+    : video_(video)
+{
+}
+
+
+Camera::Fetcher::~Fetcher()
+{
+}
+
+
+void Camera::Fetcher::fetch()
+{
+    std::lock_guard<std::mutex> lock(mutex);
+    if (video_.isOpened()) video_ >> image_;
+}
+
+
+
+// ---
+
+
+
+Camera::SyncFetcher::SyncFetcher(cv::VideoCapture &video)
+    : Fetcher(video)
+{
+}
+
+
+cv::Mat Camera::SyncFetcher::get()
+{
+    fetch();
+    return image_;
+}
+
+
+
+// ---
+
+
+Camera::AsyncFetcher::AsyncFetcher(cv::VideoCapture &video)
+    : Fetcher(video)
+    , isRunning_(false)
+    , fps_(30)
+{
+    using namespace std::chrono;
+
+    thread_ = std::thread([&] {
+        isRunning_ = true;
+        while (isRunning_) {
+            const auto t1 = high_resolution_clock::now();
+            fetch();
+            const auto t2 = high_resolution_clock::now();
+            const auto dt = duration_cast<microseconds>(t2 - t1);
+
+            auto waitTime = microseconds(1000000 / fps_) - dt;
+            if (waitTime < microseconds::zero()) waitTime = microseconds::zero();
+
+            std::this_thread::sleep_for(waitTime);
+        }
+    });
+}
+
+
+Camera::AsyncFetcher::~AsyncFetcher()
+{
+    if (thread_.joinable()) {
+        isRunning_ = false;
+        thread_.join();
+    }
+}
+
+
+cv::Mat Camera::AsyncFetcher::get()
+{
+    return image_;
+}
+
+
+
+// ---
+
+
+
 Camera::Camera(QQuickItem *parent)
     : Image(parent)
     , camera_(0)
-    , fps_(30)
+    , fps_(15)
+    , isAsync_(true)
 {
 }
 
@@ -24,12 +121,16 @@ void Camera::open()
     if ( !video_.open(camera_) ) {
         const auto msg = QString("try to open camera ") + camera_ + ", but failed...";
         error(msg);
+    } else {
+        setAsync(isAsync_);
     }
 }
 
 
 void Camera::close()
 {
+    std::lock_guard<std::mutex> lock(mutex);
+
     if ( video_.isOpened() ) {
         video_.release();
     }
@@ -42,11 +143,37 @@ bool Camera::isOpened() const
 }
 
 
-void Camera::paint(QPainter *painter){
-    if ( isOpened() ) {
-        cv::Mat img;
-        video_ >> img;
-        setImage(img);
+void Camera::fetch()
+{
+    if ( isOpened() && fetcher_ ) {
+        setImage(fetcher_->get());
     }
+}
+
+
+void Camera::paint(QPainter *painter)
+{
+    fetch();
     Image::paint(painter);
+}
+
+
+bool Camera::isAsync() const
+{
+    return isAsync_;
+}
+
+
+void Camera::setAsync(bool isAsync)
+{
+    if (isAsync_ == isAsync && fetcher_) return;
+    isAsync_ = isAsync;
+
+    if (isAsync) {
+        fetcher_ = std::make_shared<AsyncFetcher>(video_);
+    } else {
+        fetcher_ = std::make_shared<SyncFetcher>(video_);
+    }
+
+    emit isAsyncChanged();
 }
--- opencv/camera.h
+++ opencv/camera.h
@@ -2,34 +2,78 @@
 #define CAMERA_H
 
 #include "image.h"
+#include <thread>
 
 
 namespace Littai
 {
+
 
 class Camera : public Image
 {
     Q_OBJECT
     Q_PROPERTY(int fps MEMBER fps_ NOTIFY fpsChanged)
     Q_PROPERTY(int camera MEMBER camera_ NOTIFY deviceChanged)
+    Q_PROPERTY(bool isAsync READ isAsync WRITE setAsync NOTIFY isAsyncChanged)
 
 public:
+    class Fetcher
+    {
+    public:
+        explicit Fetcher(cv::VideoCapture& video);
+        virtual ~Fetcher();
+        void fetch();
+        virtual cv::Mat get() = 0;
+
+    protected:
+        cv::VideoCapture& video_;
+        cv::Mat image_;
+    };
+
+    class SyncFetcher : public Fetcher
+    {
+    public:
+        explicit SyncFetcher(cv::VideoCapture& video);
+        cv::Mat get() override;
+    };
+
+    class AsyncFetcher : public Fetcher
+    {
+    public:
+        explicit AsyncFetcher(cv::VideoCapture& video);
+        ~AsyncFetcher();
+        cv::Mat get() override;
+        void setFps(int fps) { fps_ = fps; }
+
+    private:
+        std::thread thread_;
+        bool isRunning_;
+        int fps_;
+    };
+
     explicit Camera(QQuickItem *parent = nullptr);
     ~Camera();
     void paint(QPainter *painter) override;
 
+    bool isAsync() const;
+    void setAsync(bool isAsync);
+
     Q_INVOKABLE void open();
     Q_INVOKABLE void close();
+    Q_INVOKABLE void fetch();
     Q_INVOKABLE bool isOpened() const;
 
 private:
     cv::VideoCapture video_;
     int camera_;
     int fps_;
+    std::shared_ptr<Fetcher> fetcher_;
+    bool isAsync_;
 
 signals:
     void fpsChanged() const;
     void deviceChanged() const;
+    void isAsyncChanged() const;
 };
 
 }
